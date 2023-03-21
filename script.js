a822de277246a1d7b7c6c65f3f295b419a62fa4c
faboubijou <124534182+faboubijou@users.noreply.github.com>
2023-03-21 17:06:00 +0100

    Squares OK

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const restart = document.querySelector("#initial");
-//const container = document.querySelector(".container");
 
 function numberCase() {
     let number = prompt("Give the number of cases for a side (above 0 and under 100)",16);
     while (number > 100 || number<1) {
         number = prompt("You really need to give a number < 100 and > 0. Try again");
     }
-    clearContainer;
+    clearContainer();
     addCases(number);
 }
 
@@ -19,12 +18,18 @@ function clearContainer() {
 
 function addCases(number) {
     const container = document.querySelector(".container");
-    let number2 = number*number;
-    for (i=0;i<number2;i++) {
-        const childAdd = document.createElement('div');
+    for (i = 0;i<number;i++) {
+        const childAdd = document.createElement("div");
         childAdd.classList.add("childAdd");
+        for (j=0;j<number;j++) {
+            const subChildAdd = document.createElement("div");
+            subChildAdd.classList.add("subChildAdd");
+            childAdd.appendChild(subChildAdd);
+        }
         container.appendChild(childAdd);
     }
 }
 
 restart.addEventListener("click",numberCase);
+
+
