523a0ae3b3ad21b4a48d7a57f7b449731a44a3a9
faboubijou <124534182+faboubijou@users.noreply.github.com>
2023-03-22 01:50:05 +0100

    End project Etch a Sketch

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function clearContainer() {
 
 function addCases(number) {
     const container = document.querySelector(".container");
-    for (i = 0;i<number;i++) {
+    for (i=0;i<number;i++) {
         const childAdd = document.createElement("div");
         childAdd.classList.add("childAdd");
         for (j=0;j<number;j++) {
@@ -28,8 +28,23 @@ function addCases(number) {
         }
         container.appendChild(childAdd);
     }
+    const subChildAdds = document.querySelectorAll(".subChildAdd");
+    subChildAdds.forEach(subChildAdd => subChildAdd.addEventListener('mouseenter',function(e) {
+        if (getComputedStyle(subChildAdd)["backgroundColor"] === "rgba(0, 0, 0, 0)") {
+            let r = Math.floor(255*Math.random());
+            let g = Math.floor(255*Math.random());
+            let b = Math.floor(255*Math.random());
+            subChildAdd.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        } else {
+            let presentColor = getComputedStyle(subChildAdd)["backgroundColor"];
+            presentColor = presentColor.slice(4,presentColor.length-1);
+            let valeurs = presentColor.split(", ");
+            let r = Math.max(0,valeurs[0]-25);
+            let g = Math.max(0,valeurs[1]-25);
+            let b = Math.max(0,valeurs[2]-25);
+            subChildAdd.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        }
+    }));
 }
 
 restart.addEventListener("click",numberCase);
-
-
